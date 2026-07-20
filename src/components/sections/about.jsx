476bb212122bf93f0d24a84c54e28e05ad45273c
Sakey <sheikh36ahmed@gmail.com>
2026-07-20 16:54:54 +0100

    updated about

--- src/components/sections/about.jsx
+++ src/components/sections/about.jsx
@@ -7,20 +7,18 @@ const About = () => {
         <h2>About</h2>
         <p className="paragraph">I'm a third-year computer science student, raised in the UK</p>
         <p className="paragraph">
-          <strong>Here's the deal,</strong> I've worked on a range of projects, from collaborating
-          on an e-commerce store for gluten-free products, to designing my own portfolio website to
-          building my own chrome-extension.
+          <strong>Here's the deal,</strong> I've worked on a range of projects, from engineering a tutoring website
+          that handles payments, booking and auth to building a website for a local bakery of mine.
         </p>
         <p className="paragraph">And that's just the start.</p>
         <p className="paragraph">
-          I'm here because I like building and want to solve real problems.
+          I'm here because I like solving problems and developing is just one part of that.
         </p>
-        <p className="paragraph">Two birds, one stone.</p>
         <p className="paragraph">
           When I'm not coding, I'm either working out, rock climbing, or hanging out with friends.
         </p>
         <p className="paragraph" style={{ textDecoration: "line-through" }}>
-          But you'll probably just find me binging the latest show on Netflix.
+          But you'll probably just find me binging Breaking Bad.
         </p>
         <p className="paragraph">
           P.S: I'm also active on{" "}
